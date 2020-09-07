138a19e3ad55db9a2f37b085a695f621d43a549c
András Ács <acsandras@gmail.com>
2020-09-07 11:22:07 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,20 @@
+import org.w3c.dom.ls.LSOutput;
+
+/**
+ * Developed by András Ács (acsandras@gmail.com)
+ * Zealand / www.zealand.dk
+ * Licensed under the MIT License
+ * 07/09/2020
+ */
+
+public class Hello {
+    // psvm
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Hello World!");
+        System.out.println("Hello Mor!");
+        System.out.println(5);
+        System.out.println("Hej Far");
+
+    }
+}
